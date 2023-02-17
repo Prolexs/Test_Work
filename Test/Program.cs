cd4ee63fb54e891280aebaa5249e74a1a9bbc55d
Алексей Старченко <Prolex>
2023-02-17 18:30:13 +0300

    задали массив строк и метод поиска искомых элементов

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] text1 = new string[4] {"hi", "hello", "world", "education"};
+string[] text2 = new string[text1.Length];
+void FillText2(string[] text1, string[] text2)
+{
+    int count = 0;
+    for (int i = 0; i < text1.Length; i++)
+    {
+    if(text1[i].Length <= 3)
+        {
+        text2[count] = text1[i];
+        count++;
+        }
+    }
+}
+
