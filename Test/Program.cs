9bf8214e7e5f3e00b98bac0db79d5803f605fbc7
Алексей Старченко <Prolex>
2023-02-17 18:31:48 +0300

    написали метод вывода изначального и конечного массива строк

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] text1 = new string[4] {"hi", "hello", "world", "education"};
+﻿string[] text1 = new string[4] {"hi", "hello", "day", "education"};
 string[] text2 = new string[text1.Length];
 void FillText2(string[] text1, string[] text2)
 {
@@ -13,3 +13,14 @@ void FillText2(string[] text1, string[] text2)
     }
 }
 
+void PrintArray(string[] text)
+{
+    for (int i = 0; i < text.Length; i++)
+    {
+        Console.Write($"{text[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(text1);
+FillText2(text1, text2);
+PrintArray(text2);
